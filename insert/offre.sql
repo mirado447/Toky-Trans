d4e648f8c1a6a0e2b48c3e97a4972eca3da0e658
Sarobidy-23 <hei.sarobidy@gmail.com>
2022-06-03 21:40:32 +0300

    add insert

---
+++ insert/offre.sql
@@ -0,0 +1,12 @@
+insert into offre (type, tarif) values ('Lite',25000);   --Toamasina--
+insert into offre (type, tarif) values ('Premium','35000');
+insert into offre (type, tarif) values ('VIP','60000');
+insert into offre (type, tarif) values ('Lite','40000');   --Majunga--
+insert into offre (type, tarif) values ('Premium','60000');
+insert into offre (type, tarif) values ('VIP','90000');
+insert into offre (type, tarif) values ('Lite','30000');   --Fianarantsoa--
+insert into offre (type, tarif) values ('Premium','45000');
+insert into offre (type, tarif) values ('VIP','70000');
+insert into offre (type, tarif) values ('Lite','90000');  --Antsiranana--
+insert into offre (type, tarif) values ('Premium','110000');
+insert into offre (type, tarif) values ('VIP','140000');
---
+++ insert/utiliser.sql
@@ -0,0 +1,8 @@
+insert into utiliser (id_vehicule, id_voyage) values (1, 161);
+insert into utiliser (id_vehicule, id_voyage) values (4, 161);
+insert into utiliser (id_vehicule, id_voyage) values (2, 130);
+insert into utiliser (id_vehicule, id_voyage) values (5, 130);
+insert into utiliser (id_vehicule, id_voyage) values (3, 118);
+insert into utiliser (id_vehicule, id_voyage) values (6, 118);
+insert into utiliser (id_vehicule, id_voyage) values (42, 17);
+insert into utiliser (id_vehicule, id_voyage) values (43, 17);
