baf699ac9c7a97b80e150e8bc01cd298483ccde3
RATSIMBAZAFY Anjaraniaina <hei.anjaraniaina@gmail.com>
2022-06-09 01:35:31 +0300

    fix: added more request

---
+++ Requests-Response/Line31.sql
@@ -0,0 +1,2 @@
+-- le nombre de place non occuper a chaque depart
+select id_vehicule,places_dispo from vehicules where id_vehicule=3;
--- Requests-Response/Line43.sql
+++ Requests-Response/Line43.sql
@@ -1,7 +1,2 @@
--- Ligne 43 : lister les véhicules qui partent à un certain moment
-    --avec chauffeur
-    SELECT id_vehicule,nom_complet FROM vehicule 
-        INNER JOIN chauffeur ON vehicule.id_vehicule=chauffeur.id_vehicule
-        INNER JOIN utiliser ON utiliser.id_vehicule=vehicule.id_vehicule
-        INNER JOIN voyage ON utiliser.id_voyage=voyage.id_voyage
-        WHERE voyage.jour_depart='2022-05-20'; 
+--lister les vehicule qui part a un certain moment
+ select utiliser.id_voyage,id_vehicule from voyage,utiliser where heure_depart='07:00:00' and jour_depart='2022-05-21' and utiliser.id_voyage=voyage.id_voyage;
