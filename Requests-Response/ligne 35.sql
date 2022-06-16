3eb434361aea656832c7493221c472347551e697
hiDden rafra <98639271+HidDen-MaChinA@users.noreply.github.com>
2022-06-16 21:36:41 +0300

    feat : add ligne 35

---
+++ Requests-Response/ligne 35.sql
@@ -0,0 +1,8 @@
+ligne 35:
+select client.id_client,nom_client,prenom_client,contact from client 
+	inner join reserver 
+	inner join voyage 
+	on reserver.id_voyage=voyage.id_voyage 
+	on client.id_client=reserver.id_client 
+	where voyage.jour_depart='2021-08-10';
+-- afficher les des client qui partent aujourd'hui ou un certain jour (2021-08-10)
