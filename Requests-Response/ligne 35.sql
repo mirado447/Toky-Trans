c8dccf615f3803b1852a43eeee9eceb0e7ccefe2
hiDden rafra <98639271+HidDen-MaChinA@users.noreply.github.com>
2022-06-16 21:53:13 +0300

    fix:ligne 35

--- Requests-Response/ligne 35.sql
+++ Requests-Response/ligne 35.sql
@@ -1,9 +1,8 @@
-ligne 9:
-select vehicules.places_dispo from vehicules 
-	inner join utiliser 
+ligne 35:
+select client.id_client,nom_client,prenom_client,contact from client 
+	inner join reserver 
 	inner join voyage 
-	on voyage.id_voyage=utiliser.id_voyage 
-	on utiliser.id_vehicule=vehicules.id_vehicule 
-	where utiliser.id_vehicule=1 
-	and utiliser.id_voyage=161;
--- donner les places disponible dans le vehicule 1 pour le voyage 161
+	on reserver.id_voyage=voyage.id_voyage 
+	on client.id_client=reserver.id_client 
+	where voyage.jour_depart='2021-08-10';
+-- afficher les des client qui partent aujourd'hui ou un certain jour (2021-08-10) 
