d2c5091f0db7b60f5939700a9284093bd95d5ad0
Mirado <hei.mirado@gmail.com>
2022-06-23 21:34:54 +0200

    ajout du navbar

---
+++ navbar/src/page/Requetes.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Navbar from '../Navbar';
+const Requetes = () => {
+  return (
+    <div>
+ <Navbar/>
+    </div>
+  );
+};
+
+export default Requetes;
---
+++ navbar/src/page/Reserver.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../Navbar';
+
+const Reserver = () => {
+  return (
+    <div>
+      <Navbar/>
+    </div>
+  );
+};
+
+export default Reserver;
