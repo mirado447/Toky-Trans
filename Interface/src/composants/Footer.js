9d9492ff4bd19c617341e916d814f201e5c70953
RATSIMBAZAFY Anjaraniaina <hei.anjaraniaina@gmail.com>
2022-06-23 16:35:44 +0300

    feat: added footer

---
+++ Interface/src/composants/Footer.js
@@ -0,0 +1,38 @@
+export default function Footer(){
+    /**
+     * <link rel="stylesheet" href="./bootstrap-5.1.3-dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+     */
+
+    return (
+        <footer class="bg-light py-5">
+        <div class="container ">
+            <div class="row">
+                <div class="col-md-4">
+                    <h4>Contact</h4>
+                    <p>
+                        Adress: II J 161 R Ambodivoanjo Ivandry Antananarivo, 101 <br/>
+                        Call: +261 34 94 041 16 <br/>
+                        Email: <a href="contact@hei.school"> contact@hei.school </a>
+                    </p>
+                    </div>
+                    <div class="col-md-3">
+                    <h4>Social presense</h4>
+                    <ul class="list-unstyled">
+                        <li><a href="https://web.facebook.com/hei.school/?_rdc=1&_rdr"><i class="bi bi-facebook"></i></a> Facebook</li>
+                        <li><a href="https://github.com/Anjaraniaina/Toky-Trans/"><i class="bi bi-github"></i></a> Github</li>
+                        
+                        <li><a href="https://www.linkedin.com/company/hei-school/?originalSubdomain=mg"><i class="bi bi-linkedin"></i></a> Linkedin</li>
+                    </ul>
+                    </div>
+                    <div class="col-md-5">
+                    <p>© 2022 All rights reserved.<br/>
+                        Made with <i class="bi bi-heart-fill"></i> by <a href="https://github.com/Anjaraniaina/Toky-Trans/">a group of student</a></p>
+                    </div>
+            </div>
+        </div>
+    </footer>
+
+    ) 
+
+}
